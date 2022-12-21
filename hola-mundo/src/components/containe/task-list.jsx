0641abc127028cbc8fa515b21c90bca4172e1234
Ahecha <90638871+Ahecha@users.noreply.github.com>
2022-12-21 15:41:47 -0300

    5 files modified, 1 added

--- hola-mundo/src/components/containe/task-list.jsx
+++ hola-mundo/src/components/containe/task-list.jsx
@@ -7,9 +7,11 @@ import TaskComponent from '../pure/forms/task';
 const TaskListComponent = (props) => {
 
     
-    const defaultTask = new Task("Example", "Default description", false, LEVELS.NORMAL);
+    const defaultTask1 = new Task("Example1", "Description 1", true, LEVELS.NORMAL);
+    const defaultTask2 = new Task("Example2", "Description 2", false, LEVELS.NORMAL);
+    const defaultTask3 = new Task("Example3", "Description 3", false, LEVELS.NORMAL);
 
-    const {tasks, setTasks} = useState([defaultTask]);
+    const {tasks, setTasks} = useState([defaultTask1, defaultTask2, defaultTask3]);
 
     const {loading, setLoading} = useState(true);
 
@@ -28,12 +30,41 @@ const TaskListComponent = (props) => {
 
 
     return (
-        <div>
-            <div>
-                Your task:
+    <div>
+        <div className='col-12'>
+            <div className="card">
+                <div className='card-header p-3'>
+                    <h5>
+                    Your task:
+                    </h5>
+                </div>
             </div>
-            <TaskComponent task= {defaultTask}></TaskComponent>
+           <div className= 'card-body' data-mdb-perfect-crollbar= 'true' style={ {position: "relative", height: '400px'} }>
+
+               <table>
+                   <thead>
+                   <tr>
+                <th scope= "col">Title</th>
+                <th scope= "col">Description</th>
+                <th scope= "col">Priority</th>
+                <th scope= "col">Actions</th>
+                </tr>
+                   </thead>
+                   <tbody>
+
+        {task.map((tas, index)=> {
+            return( <TaskComponent key= {index} task={task}></TaskComponent>
+                )
+        })}
+
+                    </tbody>
+                   </table>
+
+            </div> 
+            <TaskForm></TaskForm>
         </div>
+        
+    </div>
     );
 };
 
@@ -42,3 +73,6 @@ const TaskListComponent = (props) => {
 
 
 export default TaskListComponent;
+
+
+
--- hola-mundo/src/components/forms/task.jsx
+++ hola-mundo/src/components/forms/task.jsx
@@ -12,10 +12,72 @@ const TaskComponent = ({task}) => {
         console.log(`la tarea: ${task.name} va a desaparecer`)
       }
     }, [task])
+
+
+    function taskLevelBadge() {
+        switch(task.level) {
+                case LEVEL.NORMAL:
+                    return(
+                        <h6 className="mb-0"> 
+                        <span className= "badge bg-primary"> 
+                            {task.level}
+                        </span>
+                        </h6> 
+                    )
+                
+                    case LEVEL.URGENT:
+                        return(
+                            <h6 className="mb-0"> 
+                            <span className= "badge bg-warning"> 
+                                {task.level}
+                            </span>
+                            </h6> 
+                        )
+
+                    case LEVEL.BLOCKING:
+                    return(
+                        <h6 className="mb-0"> 
+                        <span className= "badge bg-danger"> 
+                            {task.level}
+                        </span>
+                        </h6> 
+                    )
+                    default:
+                        break;
+        }
+    }
+
+    function taskIconCompleted() {
+        if(task.completed) {
+            return(<i className="bi-toggle-on" style={ {color: 'green', fontWeight:'bold'} }></i>)
+        } else {
+            return(<i className="bi-toggle-off" style={ {color: 'green', fontWeight:'bold'} }></i>)
+        }
+    }
     
 
     return (
-        <div>
+    
+        <tr className= "fw-normal">
+            <th>  
+                <span className= "ms-2"> {task.name}  </span>
+            </th>
+            <td className="align-middle"> 
+                <span>{task.description} </span> 
+            </td>
+            <td className="align-middle"> 
+                {taskLevelBadge()}
+            </td>
+            <td className="align-middle"> 
+               {taskIconCompleted()}
+                <i className="bi-trash" style={ {color: 'tomato', fontSize:'20px'} }></i>
+            </td>
+
+
+        </tr>
+    
+    
+        /*  <div>
            
             <h2>
                 Name:  {task.name}
@@ -30,7 +92,7 @@ const TaskComponent = ({task}) => {
                 This task is: {task.completed ? "Complete": "Pending"}
             </h5>
 
-        </div>
+        </div> */
     );
 };
 
